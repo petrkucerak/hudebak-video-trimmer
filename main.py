b5ca10825245d99b05ea6540c7ceb4c3d1072ab4
petrkucerak <petr@khome.cz>
2024-05-04 00:06:31 +0200

    Create rule for videos

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import sys
+import json
 
 if len(sys.argv) < 4:
-   print("ERROR", "Input shut be in format main.py <json_path> <video_path> <covers_path>")
-   exit(1)
+    print("ERROR", "Input shut be in format main.py <json_path> <video_path> <covers_path>")
+    exit(1)
 paths = {
-   "json": sys.argv[1],
-   "video": sys.argv[2],
-   "covers": sys.argv[3]
+    "json": sys.argv[1],
+    "video": sys.argv[2],
+    "covers": sys.argv[3]
 }
 
+with open(paths["json"], "r", encoding="utf-8") as f:
+    data = json.loads(f.read())
+
+print(data)
